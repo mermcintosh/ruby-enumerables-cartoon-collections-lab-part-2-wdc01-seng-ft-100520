954b572dedc19c84e944e08eb4991011045aa9ed
Meredith McIntosh <meredith@mcintosh.io>
2020-09-25 01:52:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -51,8 +51,7 @@ def find_valid_calls(planeteer_calls)
     valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
     planeteer_calls.find {|item| valid_calls.include?(item)}
 end
-def find_valid_calls(planeteer_calls)
-  
+# def find_valid_calls(planeteer_calls)
 #     valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 #     planeteer_calls.find {|item| valid_calls.include?(item)}
 # end
